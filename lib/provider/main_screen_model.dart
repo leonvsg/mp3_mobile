1fb494a202da5eb9dbdf085708db62ce3870cdcc
Vazhorov <leonvsg@gmail.com>
2021-12-14 23:53:39 +0300

    navigation bar refactoring

--- lib/provider/main_screen_model.dart
+++ lib/provider/main_screen_model.dart
@@ -1,25 +1,45 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 import 'package:mp3_mobile/ui/navigation/main_navigation.dart';
 import 'package:mp3_mobile/ui/screens/main/components/help_view_widget.dart';
 import 'package:mp3_mobile/ui/screens/main/components/orders_list_view_widget.dart';
 import 'package:mp3_mobile/ui/screens/main/components/statistic_view_widget.dart';
 
+class _BottomNavigationBarOption {
+  final String label;
+  final Icon icon;
+  final Widget widget;
+
+  const _BottomNavigationBarOption({
+    required this.label,
+    required this.icon,
+    required this.widget,
+  });
+}
+
 class MainScreenModel extends ChangeNotifier {
   var selectedTabIndex = 0;
-  final _statisticWidget = const StatisticView();
-  final _orderListWidget = const OrdersListView();
-  final _helpWidget = const HelpView();
-  late List<Widget> widgetOptions;
 
-  final appBarLables = [
-    'Статистика',
-    'Заказы',
-    'Помощь',
+  static const _bottomNavigationBarOptions = [
+    _BottomNavigationBarOption(
+      icon: Icon(Icons.stacked_line_chart_sharp),
+      label: 'Статистика',
+      widget: StatisticView(),
+    ),
+    _BottomNavigationBarOption(
+      icon: Icon(Icons.payment),
+      label: 'Заказы',
+      widget: OrdersListView(),
+    ),
+    _BottomNavigationBarOption(
+      icon: Icon(Icons.help_center),
+      label: 'Помощь',
+      widget: HelpView(),
+    ),
   ];
 
-  MainScreenModel() {
-    widgetOptions = [_statisticWidget, _orderListWidget, _helpWidget];
-  }
+  List<_BottomNavigationBarOption> get widgetOptions =>
+      _bottomNavigationBarOptions;
 
   void onSelectTab(int index) {
     if (index != selectedTabIndex) {
@@ -33,7 +53,7 @@ class MainScreenModel extends ChangeNotifier {
   }
 
   Widget? showFab(Widget fab) {
-    if (widgetOptions[selectedTabIndex] == _orderListWidget) {
+    if (widgetOptions[selectedTabIndex].widget is OrdersListView) {
       return fab;
     }
     return null;
